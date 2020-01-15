eeb6a51fc523a47fec9e47dd8c9cce83fbbe8137
Chad Torkkola <ctorkkola#gmail.com>
2020-01-14 18:53:30 -0700

    Reorganized code. Made a couple fixes

--- Program.cs
+++ Program.cs
@@ -5,59 +5,77 @@ namespace Blackjack
 {
     class Blackjack
     {
+        //Player cards is an array that holds the cards that were dealt to the user. 
         static string[] playerCards = new string[11];
+        //users to store the player choice (hit or stay)
         static string playerChoice = "";
-        static int total = 0;
-        static int  count = 1; 
+        
+        static int playerTotal = 0;
+        static int  cardCount = 1; 
         static int  dealerTotal = 0;
         static Random cardRandomizer = new Random();
+        static string playAgain ="Y";
 
         static void Main(string[] args)
         {
-          StartGame();
-          StartGameLoop();
-          Console.WriteLine("Would you like to play again? (Y)es or (N)o?");
-           PlayAgain();
+            //Main loop: playagain is always yes unless they choose no 
+            while (playAgain.ToUpper() == "Y")
+            {
+                //Deal cards and display a welcome message
+                StartGame();
+                //handle the loop which plays out the users hand (Hit or stay)
+                StartGameLoop();
+
+                Console.WriteLine("Would you like to play again? (Y)es or (N)o?");
+                PlayAgain();
+            }
         }
+        //startGane Deals the player and the dealer cards . Displays a welcome message and asks the player if they would like to hit or stay
         static void StartGame()
         {
             dealerTotal = cardRandomizer.Next(15, 22);
             playerCards[0] = DealCard();
             playerCards[1] = DealCard();
             DisplayWelcomeMessage();
-            do
-            {
-               Console.WriteLine("Would you like to (H)it or (S)tay?") ;
-                playerChoice = Console.ReadLine().ToUpper();
-            }
-            while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
-           
+            //Do this until the player chooses a valid option
+       
         }
 
         private static void DisplayWelcomeMessage()
         {
             Console.WriteLine("Welcome to Blackjack! You were dealt the cards : {0} and {1} ", playerCards[0], playerCards[1]);
-            Console.WriteLine("Your total is {0} ", total);
+            Console.WriteLine("Your playerTotall is {0} ", playerTotal);
         }
 
         static void StartGameLoop()
         {
+
+            do
+            {
+                Console.WriteLine("Would you like to (H)it or (S)tay?");
+                playerChoice = Console.ReadLine().ToUpper();
+            }
+            while (!playerChoice.Equals("H") && !playerChoice.Equals("S"));
+          
+            
             if (playerChoice.Equals("H"))
             {
+                //hit will get them a card / check the total and ask for another hit 
                 Hit();
             }
-            else if (playerChoice.Equals("S"))
+
+            if (playerChoice.Equals("S"))
             {
-                if (total > dealerTotal && total <= 21)
+                if (playerTotal > dealerTotal && playerTotal <= 21)
                 {
-                    Console.WriteLine("Congrats! You won the game! The dealer's total was {0} ", dealerTotal); 
+                    Console.WriteLine("Congrats! You won the game! The dealer's playerTotal was {0} ", dealerTotal); 
                 }
-                else if (total < dealerTotal)
+                else if (playerTotal < dealerTotal)
                 {
-                    Console.WriteLine("Sorry, you lost! The dealer's total was {0}", dealerTotal);
+                    Console.WriteLine("Sorry, you lost! The dealer's playerTotal was {0}", dealerTotal);
                 }
             }
-         // Console.ReadLine();
+         
         }
 
         static string DealCard()
@@ -74,46 +92,46 @@ namespace Blackjack
                     switch (cards)
                     {
                         case 1:
-                            Card = "Two"; total += 2;
+                            Card = "Two"; playerTotal += 2;
                             break;
                         case 2:
-                            Card = "Three"; total += 3;
+                            Card = "Three"; playerTotal += 3;
                             break;
                         case 3:
-                            Card = "Four"; total += 4;
+                            Card = "Four"; playerTotal += 4;
                             break;
                         case 4:
-                            Card = "Five"; total += 5;
+                            Card = "Five"; playerTotal += 5;
                             break;
                         case 5:
-                            Card = "Six"; total += 6;
+                            Card = "Six"; playerTotal += 6;
                             break;
                         case 6:
-                            Card = "Seven"; total += 7;
+                            Card = "Seven"; playerTotal += 7;
                             break;
                         case 7:
-                            Card = "Eight"; total += 8;
+                            Card = "Eight"; playerTotal += 8;
                             break;
                         case 8:
-                            Card = "Nine"; total += 9;
+                            Card = "Nine"; playerTotal += 9;
                             break;
                         case 9:
-                            Card = "Ten"; total += 10;
+                            Card = "Ten"; playerTotal += 10;
                             break;
                         case 10:
-                            Card = "Jack"; total += 10;
+                            Card = "Jack"; playerTotal += 10;
                             break;
                         case 11:
-                            Card = "Queen"; total += 10;
+                            Card = "Queen"; playerTotal += 10;
                             break;
                         case 12:
-                            Card = "King"; total += 10;
+                            Card = "King"; playerTotal += 10;
                             break;
                         case 13:
-                            Card = "Ace"; total += 11;
+                            Card = "Ace"; playerTotal += 11;
                             break;
                         default:
-                            Card = "2"; total += 2;
+                            Card = "2"; playerTotal += 2;
                             break;
                     }
             return Card;
@@ -121,22 +139,22 @@ namespace Blackjack
 
         static void Hit()
         {
-            count += 1;
-            playerCards[count] = DealCard();
-            Console.WriteLine("You card is  a(n) {0}  and your new total is {1}. ", playerCards[count] , total);
+            cardCount += 1;
+            playerCards[cardCount] = DealCard();
+            Console.WriteLine("You card is  a(n) {0}  and your new playerTotall is {1}. ", playerCards[cardCount] , playerTotal);
 
-            if (total.Equals(21))
+            if (playerTotal.Equals(21))
             {
-                Console.WriteLine("You got Blackjack! The dealer's total was {0}. ", dealerTotal );
+                Console.WriteLine("You got Blackjack! The dealer's playerTotall was {0}. ", dealerTotal );
                 
                
             }
-            else if (total > 21)
+            else if (playerTotal > 21)
             {
-                Console.WriteLine("You busted!  Sorry!. The dealer's total was {0}",dealerTotal );
+                Console.WriteLine("You busted!  Sorry!. The dealer's playerTotall was {0}",dealerTotal );
                
             }
-            else if (total < 21)
+            else if (playerTotal < 21)
             {
                 do
                 {
@@ -154,8 +172,8 @@ namespace Blackjack
 
         static void PlayAgain()
         {
-            string playAgain = "";
 
+            //Loop until they make a valid choice 
             do
             {
                 playAgain = Console.ReadLine().ToUpper();
@@ -168,15 +186,11 @@ namespace Blackjack
                 Console.ReadLine();
                 Console.Clear();
                 dealerTotal = 0;
-                count = 1;
-                total = 0;
-
-                StartGame();
+                cardCount = 1;
+                playerTotal = 0;
             }
             else if (playAgain.Equals("N"))
             {
-      
-
                 ConsoleKeyInfo info = Console.ReadKey();
                 if (info.Key == ConsoleKey.Enter)
                 {
